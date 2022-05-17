35fe5ef8a4181bb1465a6300b6895ade3e7bbad3
Wolfgang Holoch <wolfgang.holoch@gmail.com>
2022-05-17 10:27:40 +0200

    Disable luasnip in cmp

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -29,7 +29,7 @@ cmp.setup {
   sources = cmp.config.sources {
     {name = 'nvim_lua'},
     {name = 'nvim_lsp'},
-    {name = 'luasnip'},
+--    {name = 'luasnip'},
     {name = 'buffer'},
     {name = 'path'},
     {name = 'spell', keyword_length = 3, max_item_count = 5}
@@ -56,7 +56,7 @@ cmp.setup.cmdline(':', {
 
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-capabilities.textDocument.completion.completionItem.snippetSupport = true
+--capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 require('lspconfig')['solargraph'].setup {capabilities = capabilities}
 require('lspconfig')['html'].setup {capabilities = capabilities}
