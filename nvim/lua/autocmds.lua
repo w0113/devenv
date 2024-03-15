f12ee295be7a8dac732ea37bd00b47bd6d2cb75c
Wolfgang Holoch <wolfgang.holoch@gmail.com>
2024-03-15 11:35:03 +0100

    Renamed autocmd.lua to autocmds.lua

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -2,9 +2,7 @@ local api = vim.api
 
 -- Set filetype specific options.
 local filetype_settings = api.nvim_create_augroup('filetype_settings', {clear = true})
-api.nvim_create_autocmd(
-  'FileType',
-  {
+api.nvim_create_autocmd('FileType', {
     pattern = {'todo'},
     command = 'setlocal noautoindent nosmartindent',
     group = filetype_settings
