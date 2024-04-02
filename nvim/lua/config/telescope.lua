6f5b90ac4e5aa0ab3b70861fa8cec0aa8ad98b25
Wolfgang Holoch <wolfgang.holoch@gmail.com>
2024-04-02 10:39:48 +0200

    Added mapping for Telescope spell_suggest

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -11,9 +11,9 @@ map('n', 'sa', ':lua require("telescope.builtin").grep_string {only_sort_text = 
 map('n', 'sA', ':Telescope live_grep<CR>', default_options)
 map('n', 'sb', ':Telescope buffers<CR>', default_options)
 map('n', 'sh', ':Telescope help_tags<CR>', default_options)
-
 map('n', 'sq', ':lua require("telescope.builtin").quickfix()<CR>', default_options)
 map('n', 'sc', ':lua require("telescope.builtin").git_commits()<CR>', default_options)
 map('n', 'sf', ':lua require("telescope.builtin").lsp_document_symbols()<CR>', default_options)
 map('n', 'sF', ':lua require("telescope.builtin").lsp_workspace_symbols()<CR>', default_options)
 map('n', 'sd', ':lua require("telescope.builtin").diagnostics()<CR>', default_options)
+map('n', 'sr', ':Telescope spell_suggest<CR>', default_options)
