0db17d8461e09604f140206d505f50dc37078df2
Wolfgang Holoch <wolfgang.holoch@gmail.com>
2024-04-25 11:26:25 +0200

    Updated plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -11,5 +11,23 @@ return {
     opts = { scope = { enabled = true, show_start = false, show_end = false } },
   },
 
-  { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = { "ruby" } } },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      -- Remove the last item of lualine_c, which is a shortened file path.
+      table.remove(opts.sections.lualine_c)
+      -- Instead we want to show the current relative filename
+      table.insert(opts.sections.lualine_c, { "filename", path = 1 })
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "ruby",
+      })
+    end,
+  },
 }
