fa47d8d8cc41f318858661bcb03b9dd3ec5556bc
Wolfgang Holoch <wolfgang.holoch@mercedes-benz.com>
2024-12-17 15:41:20 +0100

    Use telecsope instead of fzf

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Neovim settings
 vim.o.textwidth = 120
 vim.o.colorcolumn = "+1"
 
@@ -11,5 +12,6 @@ vim.o.sidescrolloff = 1
 vim.o.list = false
 vim.o.listchars = "eol:¶,tab:‣ ,space:·,trail:·,extends:»,precedes:«,nbsp:␣"
 
--- Disable all animations
+-- LazyVim settings
 vim.g.snacks_animate = false
+vim.g.lazyvim_picker = "telescope"
