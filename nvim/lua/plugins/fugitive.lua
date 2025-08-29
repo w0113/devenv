de6e5c697cedeaf2b13b2f1a72a9b477e16bf631
Wolfgang Holoch <wolfgang.holoch@mercedes-benz.com>
2025-08-29 14:05:26 +0000

    Add mapping to show git file revisions

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -3,6 +3,7 @@ return {
     "tpope/vim-fugitive",
     keys = {
       { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git Diff", mode = "n" },
+      { "<leader>gr", "<cmd>0Gclog<cr>", desc = "File Revisions", mode = "n" },
     },
   },
 }
