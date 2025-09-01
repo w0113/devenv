794054b2f27d28f3ae36ed93875de952e907efb0
Wolfgang Holoch <wolfgang.holoch@mercedes-benz.com>
2025-09-01 17:14:34 +0000

    Fixed auto load bug with fugitive config

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,7 @@
 return {
   {
     "tpope/vim-fugitive",
+    cmd = "G",
     keys = {
       { "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git Diff", mode = "n" },
       { "<leader>gr", "<cmd>0Gclog<cr>", desc = "File Revisions", mode = "n" },
